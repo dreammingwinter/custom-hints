ceced801db034a0fdcfc9894871ee945a4a05a07
Vasyl Ilba <dreammingwinter@gmail.com>
2021-04-27 20:56:58 +0300

    launch

---
+++ script.js
@@ -0,0 +1,54 @@
+const hintt = document.querySelector('.hintt')
+const hintr = document.querySelector('.hintr')
+const hintb = document.querySelector('.hintb')
+const hintl = document.querySelector('.hintl')
+const hinta = document.querySelector('.hinta')
+
+addHint(hintt, 'Top', 'top')
+addHint(hintr, 'Right', 'right')
+addHint(hintb, 'Bottom', 'bottom')
+addHint(hintl, 'Left', 'left')
+
+addHint(hinta, 'Top', 'top')
+addHint(hinta, 'Right', 'right')
+addHint(hinta, 'Bottom', 'bottom')
+addHint(hinta, 'Left', 'left')
+
+function addHint(target, text, direction) {
+    target.style.position = 'relative'
+    const hint = generateHint(text, direction)
+    target.appendChild(hint)
+
+    target.addEventListener('mouseenter', () => {
+        hint.classList.add('hint-reset')
+    })
+
+    target.addEventListener('mouseleave', () => {
+        hint.classList.remove('hint-reset')
+    })
+}
+
+function generateHint(text, direction) {
+    const hintEl = document.createElement('div')
+    const arrowEl = document.createElement('span')
+    const textEl = document.createElement('div')
+
+    hintEl.classList.add('hint')
+    
+    switch (direction) {
+        case 'top': hintEl.classList.add('hint-top'); break
+        case 'bottom': hintEl.classList.add('hint-bottom'); break
+        case 'left': hintEl.classList.add('hint-left'); break
+        default: hintEl.classList.add('hint-right')
+    }
+
+    arrowEl.classList.add('arrow')
+    textEl.classList.add('text')
+
+    textEl.innerText = text
+
+    hintEl.appendChild(arrowEl)
+    hintEl.appendChild(textEl)
+
+    return hintEl
+}
